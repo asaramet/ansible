9b99886e3637c3e3c0721e1aa9969798078a15b5
Alex Saramet <alexandru@saramet.de>
2024-08-05 14:05:23 +0200

    add cron jobs

---
+++ netbox/cronjobs/aruba_8_ports.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Update netbox switches configs from synct config files
+
+cd `dirname $0`
+this_folder=$PWD
+
+exec_folder="${this_folder}/../"
+logs_folder=${this_folder}/logs/
+
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+
+cd $exec_folder
+ansible --version
+
+#ansible-playbook playbooks/sync_data.yaml | tee ${logs_folder}/sync_data.logs
+
+ansible-playbook playbooks/hp_switches.yaml --tags aruba_8,production,switches | tee ${logs_folder}/aruba_8_ports.logs &&
+ansible-playbook playbooks/hp_switches.yaml --tags aruba_8,production,trunks | tee -a ${logs_folder}/aruba_8_ports.logs &&
+ansible-playbook playbooks/hp_switches.yaml --tags aruba_8,production,interfaces | tee -a ${logs_folder}/aruba_8_ports.logs &&
+ansible-playbook playbooks/hp_switches.yaml --tags aruba_8,production,create_vlans | tee -a ${logs_folder}/aruba_8_ports.logs &&
+ansible-playbook playbooks/hp_switches.yaml --tags aruba_8,production,untagged_vlans | tee -a ${logs_folder}/aruba_8_ports.logs &&
+ansible-playbook playbooks/hp_switches.yaml --tags aruba_8,production,tagged_vlans | tee -a ${logs_folder}/aruba_8_ports.logs &&
+ansible-playbook playbooks/hp_switches.yaml --tags aruba_8,production,ip | tee -a ${logs_folder}/aruba_8_ports.logs
