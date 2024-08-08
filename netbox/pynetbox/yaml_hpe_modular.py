588e1b5c5b0d6e762082fb3236755a6015a289fe
Alex Saramet <alexandru@saramet.de>
2024-08-08 12:02:54 +0200

    collect and divide

---
+++ netbox/pynetbox/yaml_hpe_modular.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+# Collect HPE modular switches data and create yaml configs file 
+
+import re, os, yaml
+from tabulate import tabulate
+from std_functions import this_folder, main_folder
+from std_functions import config_files
+from std_functions import recursive_search, get_hostname
+from std_functions import devices_json, trunks_json, interface_names_json
+from std_functions import vlans_jason, untagged_vlans_json, tagged_vlans_json
+from std_functions import ip_addresses_json
+
+def get_modules(t_file):
+    with open(t_file, "r") as f:
+        text = f.readlines()
+
+    names = {
+        '1': 'A', '2': 'B', '3': 'C', '4': 'D', '5': 'E', '6': 'F',
+        '7': 'G', '8': 'H', '9': 'I', '10': 'J', '11': 'K', '12': 'L'
+    }
+    modules = []
+    for line in recursive_search("module", text):
+        m_list = line.split()
+        name = m_list[1]
+        if name in names.keys():
+            name = names[name]
+        modules.append({'module': name, 'type': m_list[3]})
+    return modules
+
+# return the modules json object
+def modules_json(config_files):
+    data = {'modules':[]}
+    types = {
+        "j8702a": "ProCurve 24-port 10/100/1000Base-T PoE Switch Module",
+        "j8705a": "HP J8705A ProCurve PoE 20 Port Gig-T SFP Plus 4 Port Mini GBIC ZL Module",
+        "j8706a": "ProCurve Switch 5400zl 24p Mini-GBIC Module",
+        "j8707a": "HP 4-Port 10GbE X2 ZL Module",
+        "j8765a": "ProCurve Switch VL 24-Port 10/100-TX Module",
+        "j8766a": "HP ProCurve J8766A VL 1-Port 10GbE X2 ZL Module",
+        "j8768a": "ProCurve Switch 24-port Gig-T vl Module",
+        "j9033a": "HP ProCurve Switch vl 20-Port Gig-T+ 4 SFP Module",
+        "j9534a": "Aruba J9534A",
+        "j9550a": "HP 24-Port GiG-T v2 ZL Module",
+        "j9537a": "Aruba J9537A",
+        "j9538a": "Aruba J9538A",
+        "j9731a": "Aruba 2920 2-Port 10GbE SFP+ Module",
+        "j9729a": "Aruba 2920 2-Port 10GbE SFP+ Module", # same as j9731a
+        "j9986a": "Aruba J9986A",
+        "j9990a": "Aruba J9990A",
+        "j9992a": "Aruba J9992A",
+        "j9993a": "Aruba J9993A"
+        }
+    for t_file in config_files:
+        modules = get_modules(t_file)
+        device = get_hostname(t_file)
+
+        for module in modules:
+            data['modules'].append({'device': device, 'module_bay': module['module'], 'type': types[module['type'].lower()]})
+    return data
+
+# Collect modular switches data and saved it to a YAML file
+def modular(data_folder, output_file_path, device_type_slags, devices_tags):
+    files = config_files(data_folder)
+    with open(main_folder + output_file_path, 'w') as f:
+        yaml.dump({"modular": True}, f)
+        yaml.dump(devices_json(files, device_type_slags, devices_tags), f)
+        yaml.dump(modules_json(files), f)
+        yaml.dump(trunks_json(files), f)
+        yaml.dump(interface_names_json(files), f)
+        yaml.dump(vlans_jason(files), f)
+        yaml.dump(untagged_vlans_json(files), f)
+        yaml.dump(tagged_vlans_json(files), f)
+        yaml.dump(ip_addresses_json(files), f)
+
+def main():
+    # ProCurve Modular Switches
+    data_folder = main_folder + "/data/procurve-modular/"
+    output_file_path = "/data/yaml/procurve_modular.yaml"
+
+    devices_tags = ["switch", "modular-switch"]
+
+    device_type_slags = { 
+        'J8697A': 'hpe-procurve-5406zl',
+        'J8698A': 'hpe-procurve-5412zl',
+        'J8770A': 'hpe-procurve-4204vl',
+        'J8773A': 'hpe-procurve-4208vl',
+        'J9850A': 'hpe-5406r-zl2',
+        'J9851A': 'hpe-5412r-zl2',
+        'J9729A': 'hpe-aruba-2920-48g-poep'
+    }
+
+    modular(data_folder, output_file_path, device_type_slags, devices_tags)
+
+    # Aruba Modular Switches
+    data_folder = main_folder + "/data/aruba-modular/"
+    output_file_path = "/data/yaml/aruba_modular.yaml"
+
+    devices_tags = ["switch", "modular-switch"]
+
+    device_type_slags = { 
+        'JL322A_module': ''
+    }
+
+    #modular(data_folder, output_file_path, device_type_slags, devices_tags)
+
+#---- Debugging ----#
+def debug_get_modules(data_folder):
+    table = []
+    types = set()
+    headers = ["File Name", "Modules"]
+    for f in config_files(data_folder):
+        modules = get_modules(f)
+        table.append([os.path.basename(f), modules])
+        for module in modules:
+            types.add(module['type'])
+    print(tabulate(table, headers, "github"))
+
+if __name__ == "__main__":
+    main()
+
+    #data_folder = main_folder + "/data/procurve-modular-stack/"
+    #debug_get_modules(data_folder)
--- netbox/pynetbox/yaml_stacks.py
+++ netbox/pynetbox/yaml_stacks.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-# Collect ProCurve and old HP Switches data and create a hp_modular.yaml configs file 
+# Collect HPE stacked switches data and create a yaml configs files 
 
 import re, os, yaml
 from tabulate import tabulate
 from std_functions import this_folder, main_folder
-from std_functions import serial_numbers, config_files
+from std_functions import config_files
 from std_functions import devices_json
 #from std_functions import devices_json, trunks_json, interface_names_json
 #from std_functions import vlans_jason, untagged_vlans_json, tagged_vlans_json
@@ -17,19 +17,48 @@ def stack(data_folder, output_file_path, device_type_slags, devices_tags):
     with open(main_folder + output_file_path, 'w') as f:
         yaml.dump(devices_json(files, device_type_slags, devices_tags), f)
 
+def stack_module(data_folder, output_file_path, device_type_slags, devices_tags):
+    pass
+
 def main():
-    # ProCurve 2M modular stacks
-    data_folder = main_folder + "/data/procurve-modular-stack/"
-    output_file_path = "/data/yaml/procurve_modular_stack.yaml"
+    # Aruba stacks (no extra modules)
+    data_folder = main_folder + "/data/aruba-stack/"
+    output_file_path = "/data/yaml/aruba_stack.yaml"
 
     device_type_slags = {
-        'J9729A': 'hpe-aruba-2920-48g-poep'
+        'JL256A_stack': '',
+        'JL075A_stack': '',
+        'JL693A_stack': ''
     }
 
     devices_tags = ["switch", "stack"]
 
     stack(data_folder, output_file_path, device_type_slags, devices_tags)
 
+    # HPE stacks with LWL modules
+    data_folder = main_folder + "/data/hpe-stack/"
+    output_file_path = "/data/yaml/hpe_stack.yaml"
+
+    device_type_slags = {
+        'J9729A_stack': 'hpe-aruba-2920-48g-poep',
+        'JL322A_stack': ''
+    }
+
+    devices_tags = ["switch", "stack"]
+
+    stack_module(data_folder, output_file_path, device_type_slags, devices_tags)
+
+    # Aruba modular staks
+    data_folder = main_folder + "/data/aruba-modular-stack/"
+    output_file_path = "/data/yaml/aruba_modular_stack.yaml"
+
+    device_type_slags = {
+        'J9850A_stack': ''
+    }
+
+    devices_tags = ["switch", "stack"]
+
+    #stack_module(data_folder, output_file_path, device_type_slags, devices_tags)
 
 if __name__ == "__main__":
     main()
