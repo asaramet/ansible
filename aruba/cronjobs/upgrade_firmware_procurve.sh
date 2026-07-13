ed311d9aa646b017ef61cf2d67bf072af57e9825
Alex Saramet <alexandru@saramet.de>
2026-07-13 10:27:29 +0200

    update some procurve switches

---
+++ aruba/cronjobs/upgrade_firmware_procurve.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Upgrade devices firmware
+
+SCRIPT_DIR="$(dirname $(realpath $0))"
+EXEC_DIR="$(dirname ${SCRIPT_DIR})"
+
+logs_folder="${SCRIPT_DIR}/logs"
+
+echo -e "=== Source Ansible ===\n"
+source /opt/ansible/envs
+cd /opt/ansible/inventories/aruba
+ansible --version
+
+HOSTS_GROUP='wc_2930f'
+
+echo -e "\n++ Firmware version on ${HOSTS_GROUP} before updates --\n"
+ansible-playbook playbooks/show_version_procurve.yaml -l ${HOSTS_GROUP} | tee ${logs_folder}/version_before.logs
+
+ansible-playbook playbooks/update_firmware_procurve.yaml -l ${HOSTS_GROUP} | tee ${logs_folder}/update_firmware.logs
+
+sleep 30m &&
+
+echo -e "\n++ Firmware version on ${HOSTS_GROUP} after updates --\n"
+ansible-playbook playbooks/show_version.yaml | tee ${logs_folder}/version_after.logs
