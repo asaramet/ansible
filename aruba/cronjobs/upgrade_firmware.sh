416d5744dae37f66ed31898cc643c77d656c1163
Alex Saramet <alexandru@saramet.de>
2026-07-13 09:56:48 +0200

    optimize os_cx updates

--- aruba/cronjobs/upgrade_firmware.sh
+++ aruba/cronjobs/upgrade_firmware.sh
@@ -12,16 +12,19 @@ source /opt/ansible/envs
 cd /opt/ansible/inventories/aruba
 ansible --version
 
+#HOSTS_GROUP='aruba_6xxx'
+HOSTS_GROUP='aos_cx_failed'
+
 echo -e "\n++ Firmware version before updates --\n"
-ansible-playbook playbooks/show_version.yaml --forks 20 | tee ${logs_folder}/version_before.logs
+ansible-playbook playbooks/show_version.yaml -l ${HOSTS_GROUP} | tee ${logs_folder}/version_before.logs
 
-ansible-playbook playbooks/update_firmware.yaml | tee ${logs_folder}/update_firmware.logs
+ansible-playbook playbooks/update_firmware.yaml -l ${HOSTS_GROUP} | tee ${logs_folder}/update_firmware.logs
 
-ansible-playbook playbooks/update_firmware_distri_1.yaml | tee ${logs_folder}/update_firmware_distri_1.logs
+#ansible-playbook playbooks/update_firmware_distri_1.yaml | tee ${logs_folder}/update_firmware_distri_1.logs
 
 sleep 30m &&
-ansible-playbook playbooks/update_firmware_distri_2.yaml | tee ${logs_folder}/update_firmware_distri_2.logs
+#ansible-playbook playbooks/update_firmware_distri_2.yaml | tee ${logs_folder}/update_firmware_distri_2.logs
 
 sleep 15m &&
 echo -e "\n++ Firmware version after updates --\n"
-ansible-playbook playbooks/show_version.yaml --forks 20 | tee ${logs_folder}/version_after.logs
+ansible-playbook playbooks/show_version.yaml | tee ${logs_folder}/version_after.logs
